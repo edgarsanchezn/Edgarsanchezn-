7060cbc51802b872db24f506b55959587feebf06
edgar s <edgar.sanchez.n@gmail.com>
2022-02-09 22:53:46 -0400

    added table in users

--- src/views/application/views/users.vue
+++ src/views/application/views/users.vue
@@ -1,7 +1,53 @@
 <template lang="">
-    <div>
-        Users
-        <div>{{$route.path}}</div>
+    <div class="m-10 border rounded-xl p-4">
+        <span class="font-bold text-xl">Users</span>
+        <div class="mt-4">
+            
+
+<table class="table-auto w-full ">
+  <thead class="text-left">
+    <tr>
+      <th>Id</th>
+
+      <th>First Name</th>
+      <th>Last Name</th>
+
+      <th>E-Mail</th>
+      <th>Year</th>
+    </tr>
+  </thead>
+  <tbody class="">
+    <tr>
+      <td>01</td>
+
+      <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+      <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+
+      <td>Malcolm Lockyer</td>
+      <td>1961</td>
+    </tr>
+    <tr>
+      <td>01</td>
+
+      <td>Witchy Woman</td>
+      <td>Witchy Woman</td>
+
+      <td>The Eagles</td>
+      <td>1972</td>
+    </tr>
+    <tr>
+      <td>01</td>
+
+      <td>Shining Star</td>
+      <td>Shining Star</td>
+
+      <td>Earth, Wind, and Fire</td>
+      <td>1975</td>
+    </tr>
+  </tbody>
+</table>
+
+        </div>
     </div>
 </template>
 <script>
